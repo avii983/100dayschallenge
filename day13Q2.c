74eb31b81468113d63218dd6059b116440dad372
Avi Agarwal <aviiagarwal18@gmail.com>
2025-09-02 18:45:26 +0530

    Add files via upload

---
+++ day13Q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*Q26: Write a program to print numbers from 1 to n.*/
+     int main(){ int num , t;
+                 printf("enter the value of your number:    ");
+                 scanf("%d" , &num);
+                 t = 1;
+                while (t<=num)
+                {printf("%d" , t);
+                 if (t>0)
+                 printf(" ");
+                 t++;}
+                 return 0;
+                 
+     }
+                 
+                 
+
+
+
+
+
+
+
+
+     
