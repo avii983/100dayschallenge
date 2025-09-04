be6fe74d123c04b872ba6dde9160118d6d38a484
Avi Agarwal <aviiagarwal18@gmail.com>
2025-09-04 09:33:10 +0530

    Update day15Q2.c

--- day15Q2.c
+++ day15Q2.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /*Q30: Write a program to reverse a given number.*/
- int main(){ int num , division;
+ int main(){ int num , division = 1;
              printf("enter the value of number\n");
              scanf("%d" , &num);
              while(num>0)
@@ -9,3 +9,4 @@
                 num /= 10;
              } return 0;
  }
+
