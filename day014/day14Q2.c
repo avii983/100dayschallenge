9889df1c32250a7081285a4ea609c9c728c3d5b3
Avi Agarwal <aviiagarwal18@gmail.com>
2025-09-12 18:58:14 +0530

    Add files via upload

---
+++ day014/day14Q2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/*Q28: Write a program to print the product of even numbers from 1 to n.*/
+ int main(){ int product = 1 , num , num1 = 2;
+             printf("enter the value of number\n");
+             scanf("%d" , &num);
+                 while (num1 > 0 && num1 <= num)
+                 {product *= num1;
+                 num1 += 2;}
+                 printf("the value of your product %d\n" , product);
+                return 0;
+
+
+
+
+
+
+
+
+
+
+
+ }
+ 
+ 
+
