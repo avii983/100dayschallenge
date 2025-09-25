abe4f2a7b8c39ddd4a9b49f07a0ec8635872cd20
Avi Agarwal <aviiagarwal18@gmail.com>
2025-09-25 14:27:37 +0530

    Add files via upload

---
+++ day036/day36.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/*Q71: Read and print a matrix.*/
+ int main(){ int a , b , d = 0 , e = 1 , f = 1 , g = 0;
+             printf("Enter the rows you want in your array\n");
+             scanf("%d" , &a);
+             printf("Enter the columns you want in your array\n");
+             scanf("%d" , &b);
+             int c[a][b];
+             while (e <= a)
+             {
+              while (d < b)
+              {printf("Enter the element you want in your columns c[%d][%d]\n" , e , d);
+              scanf("%d" , &c[e][d]);
+              d += 1;}
+              d = 0;
+              e += 1;
+             }
+             while (f <= a)
+             {
+               while(g < b)
+               {
+                printf("%d" , c[f][g]);
+                printf(" ");
+                g += 1;
+               }
+               printf("\n");
+               g = 0;
+               f += 1;
+             }
+             return 0;
+ }
---
+++ day036/day36Q2.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/*Q72: Find the sum of all elements in a matrix.*/
+ int main(){ int a , b , d = 0 , e = 1 , f = 0 , g = 1 , sum = 0;
+             printf("Entert the number of rows you want in your array\n");
+             scanf("%d" , &a);
+             printf("enter the number of rows you want in your array\n");
+             scanf("%d" , &b);
+             int c[a][b];
+             while (e <= a)
+             {
+                while (d < b)
+                {
+                 printf("Enter the next element you want in your array c[%d][%d]\n" , e , d);
+                 scanf("%d" , &c[e][d]);
+                 d += 1;
+                }
+                d = 0;
+                e += 1;
+             }
+             while (g <= a)
+             {
+                while (f < b)
+                {
+                 sum = sum + c[g][f];
+                 f += 1;
+                }
+                f = 0;
+                g += 1;
+             }
+            printf("The sum of all the elements present in your array is %d" , sum);
+            return 0;
+         }
