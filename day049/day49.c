1770077aa8851a66c50d0fb667b1c5eb1031bd8d
Avi Agarwal <aviiagarwal18@gmail.com>
2025-10-08 23:10:14 +0530

    Add files via upload

---
+++ day049/day49.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+/*Q97: Print the initials of a name.*/
+      int main(){ char a[1000] , b[1000];
+                  int count = 0 , ans = 0;
+                  printf("Enter the name of the person you want to enter\n");
+                  fgets(a, 1000, stdin);
+                  a[strcspn(a, "\n")] = '\0';
+                  b[ans] = a[count];
+                  ans += 1;
+                  b[ans] = '.';
+                  ans += 1;
+                  while (a[count] != '\0')
+                  {
+                    if (a[count] == 32)
+                    {
+                        b[ans] = a[count + 1];
+                        ans += 1;
+                        b[ans] = '.';
+                        ans += 1;
+                    } 
+                    count += 1;
+                  }
+                  b[ans] = '\0';
+                  printf("%s" , b);
+
+
+
+
+
+
+
+
+
+
+      }
+   
